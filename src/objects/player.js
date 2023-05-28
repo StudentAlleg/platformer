1f42d85858ebda940f0b43c9ec88e9b9c54e6852
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-05-28 00:26:01 -0700

    Sketching out structure

---
+++ src/objects/player.js
@@ -0,0 +1,5 @@
+class Player extends Phaser.GameObjects.Sprite {
+    constructor (scene, x, y, texture, frame = undefined) {
+        super(scene,x,y,texture,frame);
+    }
+}
---
+++ src/scenes/base.js
@@ -0,0 +1,3 @@
+class base extends Phaser.Scene {
+    
+}
