0243c395047d8277680763e24fb35cd1ff8eb4f8
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-06-06 20:00:18 -0700

    more comments

--- src/scenes/base.js
+++ src/scenes/base.js
@@ -26,9 +26,11 @@ class Base extends Phaser.Scene {
     }
 
     update(delta) {
-        //Owen 6/6/2023 - if the missile is flying, update it
+        
         let pointerX = this.input.activePointer.x;
         let pointerY = this.input.activePointer.y;
+
+        //Owen 6/6/2023 - if the missile is flying, update it
         if (this.missile.state == FLYING) {
             this.missile.updateMissileFromTarget(delta, pointerX, pointerY);
         }
@@ -36,7 +38,7 @@ class Base extends Phaser.Scene {
         else 
         {
             if (this.input.activePointer.isDown) {
-                 //Owen 6/2/2023 - loop through all of our buttons
+                 //Owen 6/2/2023 - if down, loop through all of our buttons to see if it is down over a button. If so, press it.
                  for (let button of this.buttons) {
                     //console.log(button);
                     //Owen 6/2/2023 - TODO, check if the active pointer is over a button, and if so, trigger that button if enough time has passed
